684687ac9908c7d6b7cbbe7f1369e136bb9d9ec8
gmw-zjw <mingwei2222@126.com>
2019-05-27 17:20:50 +0800

    修复部分bug

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexPage(),
+    return MaterialApp(
+      home: IndexPage(),
     );
   }
 }
