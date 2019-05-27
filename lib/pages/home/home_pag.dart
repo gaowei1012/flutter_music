9642d06282c10990f08fae23af8b6a13672533b3
gmw-zjw <mingwei2222@126.com>
2019-05-27 18:51:33 +0800

    banner请求数据测试

--- lib/pages/home/home_pag.dart
+++ lib/pages/home/home_pag.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_music/service/request.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    _getBannerInfo();
     return Container(
       child: Center(
         child: Text('home page'),
       )
     );
+  }
+
+  void _getBannerInfo() async {
+    await request('banner').then((val) {
+      print('banner: =====> ${val}');
+    });
   }
 }
